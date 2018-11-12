f866297e34fdb6ef504be1b74a91d0fdccc713c8
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-11-12 22:12:45 +0200

    Added register view and AccountController.

---
+++ Chushka-ASP/Chushka.Web/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿namespace Chushka.Web.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
