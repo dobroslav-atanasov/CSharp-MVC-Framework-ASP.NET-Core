bd3d6de830c4c9d12c1fc3bdbb97186a0d395277
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-11-12 19:13:08 +0200

    Added Chushka.Data.

---
+++ Chushka-ASP/Chushka.Data/ChushkaDbContext.cs
@@ -0,0 +1,27 @@
+﻿namespace Chushka.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class ChushkaDbContext : IdentityDbContext<User>
+    {
+        public ChushkaDbContext()
+        {
+        }
+
+        public ChushkaDbContext(DbContextOptions<ChushkaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+        }
+    }
+}
