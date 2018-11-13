78590768feb1824af4590f01f4ceac5efd02b01a
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-11-13 23:20:33 +0200

    Added view models for register and login.

---
+++ Chushka-ASP/Chushka.Web/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Chushka.Web.ViewModels.Account
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class LoginViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
