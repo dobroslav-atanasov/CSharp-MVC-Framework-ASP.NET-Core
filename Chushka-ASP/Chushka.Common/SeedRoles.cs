543cab9bfb44fd848bde3f8d841e9d5a1b52aeeb
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-11-13 20:39:00 +0200

    Added new project - Chushka.Common. Implemented method for seeding roles.

---
+++ Chushka-ASP/Chushka.Common/SeedRoles.cs
@@ -0,0 +1,25 @@
+﻿namespace Chushka.Common
+{
+    using System;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class SeedRoles
+    {
+        public static void Seed(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+            var adminRoleExists = roleManager.RoleExistsAsync("Admin").Result;
+            var userRoleExists = roleManager.RoleExistsAsync("User").Result;
+            if (!adminRoleExists)
+            {
+                roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+            if (!userRoleExists)
+            {
+                roleManager.CreateAsync(new IdentityRole("User"));
+            }
+        }
+    }
+}
