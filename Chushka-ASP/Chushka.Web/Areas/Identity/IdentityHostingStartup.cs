8eb782e700da611630eb057f212af2bc8ddb164a
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-11-13 23:21:13 +0200

    Added razor pages.

---
+++ Chushka-ASP/Chushka.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Chushka.Data;
+using Chushka.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Chushka.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Chushka.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
