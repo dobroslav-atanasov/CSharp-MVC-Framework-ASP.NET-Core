b71a0302f2672b24f30f9c193bc6173e9afa2a5d
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-11-14 19:41:24 +0200

    Added new migration. Changed in table Orders User's id.

--- Chushka-ASP/Chushka.Data/Migrations/20181114164632_ChangeInOrdersUserId.Designer.cs
+++ Chushka-ASP/Chushka.Data/Migrations/20181114164632_ChangeInOrdersUserId.Designer.cs
@@ -4,14 +4,16 @@ using Chushka.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Chushka.Data.Migrations
 {
     [DbContext(typeof(ChushkaDbContext))]
-    partial class ChushkaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181114164632_ChangeInOrdersUserId")]
+    partial class ChangeInOrdersUserId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Chushka-ASP/Chushka.Data/Migrations/20181114164632_ChangeInOrdersUserId.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chushka.Data.Migrations
+{
+    public partial class ChangeInOrdersUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_ClientId1",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_ClientId1",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId1",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "Orders",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ClientId",
+                table: "Orders",
+                column: "ClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_ClientId",
+                table: "Orders",
+                column: "ClientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_ClientId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_ClientId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClientId",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientId1",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ClientId1",
+                table: "Orders",
+                column: "ClientId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_ClientId1",
+                table: "Orders",
+                column: "ClientId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
