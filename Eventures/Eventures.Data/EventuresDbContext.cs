6bf4cbd07785830a2b02f205666c4563bade42b8
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-11-15 19:47:36 +0200

    Uploaded project Eventures.

---
+++ Eventures/Eventures.Data/EventuresDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Eventures.Data
+{
+    using Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class EventuresDbContext : IdentityDbContext<User>
+    {
+        public EventuresDbContext(DbContextOptions<EventuresDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
