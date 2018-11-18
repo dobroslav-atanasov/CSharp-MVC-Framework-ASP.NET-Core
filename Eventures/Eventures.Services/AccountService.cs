960a4f486128efe26913b266e3f8025a2c39df6a
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-11-18 17:49:07 +0200

    Added AccountService.cs and implemented logic for register, login and logout.

---
+++ Eventures/Eventures.Services/AccountService.cs
@@ -0,0 +1,67 @@
+﻿namespace Eventures.Services
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Interfaces;
+    using Microsoft.AspNetCore.Identity;
+    using Models;
+
+    public class AccountService : IAccountService
+    {
+        private readonly SignInManager<User> signInManager;
+        private readonly UserManager<User> userManager;
+
+        public AccountService(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
+        public async Task<bool> Register(string username, string password, string confirmPassword, string email, string firstName, string lastName, string uniqueCitizenNumber)
+        {
+            if (password != confirmPassword)
+            {
+                return false;
+            }
+
+            var user = new User
+            {
+                UserName = username,
+                Email = email,
+                FirstName = firstName,
+                LastName = lastName,
+                UniqueCitizenNumber = uniqueCitizenNumber
+            };
+
+            var result = this.userManager.CreateAsync(user, password).Result;
+
+            if (result.Succeeded)
+            {
+                if (this.userManager.Users.Count() == 1)
+                {
+                    await this.userManager.AddToRoleAsync(user, "Admin");
+                }
+                else
+                {
+                    await this.userManager.AddToRoleAsync(user, "User");
+                }
+
+                this.signInManager.SignInAsync(user, false).Wait();
+            }
+
+            return result.Succeeded;
+        }
+
+        public bool Login(string username, string password, bool rememberMe)
+        {
+            var result = this.signInManager.PasswordSignInAsync(username, password, rememberMe, true).Result;
+
+            return result.Succeeded;
+        }
+
+        public void Logout()
+        {
+            this.signInManager.SignOutAsync().Wait();
+        }
+    }
+}
---
+++ Eventures/Eventures.Services/Interfaces/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿namespace Eventures.Services.Interfaces
+{
+    using System.Threading.Tasks;
+
+    public interface IAccountService
+    {
+        Task<bool> Register(string username, string password, string confirmPassword, string email, string firstName, string lastName, string uniqueCitizenNumber);
+
+        bool Login(string username, string password, bool rememberMe);
+
+        void Logout();
+    }
+}
