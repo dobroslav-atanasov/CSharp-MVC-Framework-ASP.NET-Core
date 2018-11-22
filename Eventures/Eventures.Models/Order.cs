fd89b5a009e8afc9d2cb5c3f807ca16d7ebaf7f6
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-11-22 21:54:21 +0200

    Added new model Order.

---
+++ Eventures/Eventures.Models/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Eventures.Models
+{
+    using System;
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public DateTime OrderedOn { get; set; }
+
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        public string CustomerId { get; set; }
+        public User Customer { get; set; }
+
+        public int TicketsCount { get; set; }
+    }
+}
