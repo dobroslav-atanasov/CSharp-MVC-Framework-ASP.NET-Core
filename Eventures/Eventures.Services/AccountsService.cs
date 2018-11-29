300db8a2698f17287c10854bf74973eeea4c5eb8
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-11-29 21:24:37 +0200

    Added methods of promote and demote users.

--- Eventures/Eventures.Services/AccountsService.cs
+++ Eventures/Eventures.Services/AccountsService.cs
@@ -1,20 +1,24 @@
 ﻿namespace Eventures.Services
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Data;
     using Interfaces;
     using Microsoft.AspNetCore.Identity;
     using Models;
 
-    public class AccountService : IAccountService
+    public class AccountsService : IAccountsService
     {
         private readonly SignInManager<User> signInManager;
         private readonly UserManager<User> userManager;
+        private readonly EventuresDbContext context;
 
-        public AccountService(SignInManager<User> signInManager, UserManager<User> userManager)
+        public AccountsService(SignInManager<User> signInManager, UserManager<User> userManager, EventuresDbContext context)
         {
             this.signInManager = signInManager;
             this.userManager = userManager;
+            this.context = context;
         }
 
         public async Task<bool> Register(string username, string password, string confirmPassword, string email, string firstName, string lastName, string uniqueCitizenNumber)
@@ -62,6 +66,39 @@
         public void Logout()
         {
             this.signInManager.SignOutAsync().Wait();
+        }
+
+        public ICollection<User> GetAllUsers()
+        {
+            var users = this.context
+                .Users
+                .ToList();
+
+            return users;
+        }
+
+        public void PromoteUser(string userId)
+        {
+            var user = this.userManager.Users.FirstOrDefault(u => u.Id == userId);
+
+            var role = this.userManager.GetRolesAsync(user).GetAwaiter().GetResult();
+
+            var userRole = this.context.UserRoles.FirstOrDefault(ur => ur.UserId == userId);
+            this.context.UserRoles.Remove(userRole);
+
+            this.userManager.AddToRoleAsync(user, "Admin").GetAwaiter().GetResult();
+        }
+
+        public void DemoteUser(string userId)
+        {
+            var user = this.userManager.Users.FirstOrDefault(u => u.Id == userId);
+
+            var role = this.userManager.GetRolesAsync(user).GetAwaiter().GetResult();
+
+            var userRole = this.context.UserRoles.FirstOrDefault(ur => ur.UserId == userId);
+            this.context.UserRoles.Remove(userRole);
+
+            this.userManager.AddToRoleAsync(user, "User").GetAwaiter().GetResult();
         }
     }
 }
--- Eventures/Eventures.Services/Interfaces/IAccountsService.cs
+++ Eventures/Eventures.Services/Interfaces/IAccountsService.cs
@@ -1,13 +1,21 @@
 ﻿namespace Eventures.Services.Interfaces
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Models;
 
-    public interface IAccountService
+    public interface IAccountsService
     {
         Task<bool> Register(string username, string password, string confirmPassword, string email, string firstName, string lastName, string uniqueCitizenNumber);
 
         bool Login(string username, string password, bool rememberMe);
 
         void Logout();
+
+        ICollection<User> GetAllUsers();
+
+        void PromoteUser(string userId);
+
+        void DemoteUser(string userId);
     }
 }
